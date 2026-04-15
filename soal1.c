63ca42c25896d3b2ea6b28faa11f61f34a071220
IsnayaKamila <Isnayazalfa@gmail.com>
2026-04-15 09:13:27 +0700

    Implement countdown function in soal1.c
    Added a countdown function to recursively print numbers down to zero.

--- soal1.c
+++ soal1.c
@@ -1,5 +1,23 @@
-#include <stdio.h>
+// PRAKTIKUM PEMECAHAN MASALAH DENGAN PEMROGAMAN 
+// MODUL    : 01
+// PEMBUAT  : Isnaya Zalfa Kamila (13224028)
+// DESKRIPSI: Menentukan jumlah cara turun turun tangga 
 
-int main(void) {
-    return 0;
+#include <stdio.h> 
+#include <math.h>
+
+void countdown(int N) { // turun anak tangga 
+  if (N == 0) {  // inisiasi sudah ada di tangga terakhir 
+    printf("Done!\n");
+  } else {
+    printf("%d\n", N);
+    countdown(N - 1);   // recursive call
+  }
+}
+
+int main() {
+  int N;
+  scanf("%d", &N);
+  countdown(N);
+  return 0;
 }
